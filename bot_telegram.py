7a08d9729a0c523c6f4f9218fa9a44ca3904883b
Jefferson Roque <jefpsousa@gmail.com>
2018-06-28 23:01:56 -0300

    face_recognizer

--- bot_telegram.py
+++ bot_telegram.py
@@ -3,18 +3,18 @@ import telepot
 class bot:
 
     def __init__(self):
-        self.token = telepot.Bot("516085535:AAHhUYJPdnX96_L17ysCYI23g47HuRH1-gg")
+        self.token = telepot.Bot(" ")
 
     def msg(self, msg):
         try:
-            self.token.sendMessage(131443674, msg)
+            self.token.sendMessage( , msg)
 
         except Exception as ex:
             print(ex)
 
     def img(self, img):
         try:
-            self.token.sendPhoto(131443674, open(img, 'rb'))
+            self.token.sendPhoto( , open(img, 'rb'))
 
         except Exception as ex:
             print(ex)
